de0250fbcb29a11a1abaa84b3109614dc255e38e
Dhiman-22 <dimanvivek1697@gmail.com>
2022-12-17 22:43:53 +0530

    Revert "completed"
    This reverts commit ab1051f7470838ffa0abe066ecd59da2c2aff75c.

--- Hair/hair.js
+++ Hair/hair.js
@@ -26,19 +26,6 @@ function filteredMakeupData(data){
   })
 }
 
-// SORT FUNCTIONALITY..............
-let pop=document.querySelector("#sort_price");
-pop.addEventListener("change",function(){
- let selected=document.querySelector("#sort_price").value;
- if(selected=="High"){
-   data.sort((a,b)=>b.price-a.price);
- }
- if(selected=="Low"){
-   data.sort((a,b)=>b.price-a.price);
- }
- filteredMakeupData(data);
-});
-
 let Footer_part = document.getElementById("footer")
 Footer_part.innerHTML=footer()
 
