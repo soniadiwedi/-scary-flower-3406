f20ce9f2f124b4f5dd5937cfa0d0d71ae59b311e
Dhiman-22 <dimanvivek1697@gmail.com>
2022-12-18 15:36:30 +0530

    Day-5

--- Hair/hair.js
+++ Hair/hair.js
@@ -4,6 +4,7 @@ async function func(){
     let res = await fetch(`https://6398167e77359127a046d08d.mockapi.io/products`)
     let data = await res.json();
     filteredMakeupData(data)
+    sortingAllData(data)
   }
 func()
 function filteredMakeupData(data){
@@ -25,6 +26,47 @@ function filteredMakeupData(data){
     data_div.append(div)
   })
 }
+
+
+//<-------for sorting data------->
+function sortingAllData(data){
+  // SORT FUNCTIONALITY..............
+let pop=document.querySelector("#sort_price");
+pop.addEventListener("change",function(event){
+  
+ let selected=document.querySelector("#sort_price").value;
+ console.log(selected)
+ let data_div = document.getElementById("right_block");
+ if(selected=="Default"){
+   window.location.reload()
+ }
+ else if(selected=="High"){  
+  data_div.innerHTML = "";
+   data.sort((a,b)=>b.price-a.price);
+ }
+ else if(selected=="Low"){
+  data_div.innerHTML = "";
+   data.sort((a,b)=>a.price-b.price);
+ }
+ 
+ data.forEach((el)=>{
+  let div = document.createElement("div")
+  let image = document.createElement("img")
+  image.src = el.image;
+  let p = document.createElement("p");
+  p.innerHTML = `Name:${el.name}`
+  let p1 =document.createElement("p");
+  p1.innerHTML = `Price:${el.price}`
+  let btn = document.createElement("button")
+  btn.innerText = "Add To Cart"
+  let fav_img = document.createElement("img")
+  fav_img.src="https://assets.ajio.com/static/img/wishlistIcon.svg"
+  div.append(image,p,p1,btn,fav_img)
+  data_div.append(div)
+})
+});
+}
+
 
 let Footer_part = document.getElementById("footer")
 Footer_part.innerHTML=footer()
--- favorite/fav.js
+++ favorite/fav.js
@@ -2,7 +2,7 @@ import { footer } from "../components/navbar.js";
 
 
 
-let cartItem = JSON.parse(localStorage.getItem("fav_data"));
+let fav_data = JSON.parse(localStorage.getItem("favCart"));
 
   let totalPrice=0;
 for(let el of cartItem){
@@ -13,10 +13,10 @@ let order = document.getElementById("right_block")
 
   display(fav_data)
 
- function display(data){
+ function display(fav_data){
     document.querySelector("#middle_block").innerText="";
    
-    data.forEach((items,i)=>{
+    fav_data.forEach((items,i)=>{
     
         let div1 = document.createElement("div")
 
