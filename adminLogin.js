be1818f821e1a05581a426d2385d333ab0606410
anshuman2608 <asingh851127@gmail.com>
2022-12-18 20:40:00 +0530

    day4

---
+++ adminLogin.js
@@ -0,0 +1,14 @@
+//code for matching the admin userID and Admin password
+
+let admin_login_btn = document.querySelector("#adminLoginBtn");
+admin_login_btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let admin_login_input = document.querySelectorAll("form input");
+  let username = admin_login_input[0].value;
+  let password = admin_login_input[1].value;
+  if (username == "singh" && password == "singh") {
+    window.location.href = "./admin.html";
+  }
+  console.log(username);
+  console.log("I am being clicked");
+});
